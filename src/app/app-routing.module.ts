dc411a0011bf43b6324efc1f4e0f85b4abb7e102
Mace, Noel <noel.mace@ext.saint-gobain.com>
2017-05-14 01:47:44 +0200

    first steps

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { HomeComponent } from './core/home/home.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { useHash: true }),
+  ],
+  exports: [
+    RouterModule,
+  ]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from './core/core.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -12,7 +14,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    CoreModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
