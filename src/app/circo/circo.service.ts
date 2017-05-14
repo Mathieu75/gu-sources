08cba9464ec85b510160799264e97f2eb175c60e
Mace, Noel <noel.mace@ext.saint-gobain.com>
2017-05-14 14:02:55 +0200

    lien vers le spreadsheet circo en commentaire

--- src/app/circo/circo.service.ts
+++ src/app/circo/circo.service.ts
@@ -9,6 +9,7 @@ export class CircoService {
   private data: Promise<any>;
 
   constructor(private http: Http) {
+    // lien vers le document : https://docs.google.com/spreadsheets/d/1gYZNVhUxGogX-8WnVLvWoWiaUg0r0seAOK0AbCoEKPE/edit?usp=sharing
     this.data = http.get('https://spreadsheets.google.com/feeds/list/1gYZNVhUxGogX-8WnVLvWoWiaUg0r0seAOK0AbCoEKPE/1/public/values?alt=json')
       .map(res => res.json().feed.entry)
       .toPromise();
